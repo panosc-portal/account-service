cafa322407fa37ac5b6a9716301f54e043b0a1e8
Eric Pellegrini <pellegrini@.ill.fr>
2020-02-26 17:09:09 +0100

    completed me endpoint

--- src/controllers/me.controller.ts
+++ src/controllers/me.controller.ts
@@ -1,23 +1,90 @@
-import {get, getModelSchemaRef} from '@loopback/rest';
-import {BaseController} from './base.controller';
-import {User} from '../models';
+import { inject } from '@loopback/core';
+import { get, getModelSchemaRef, HttpErrors, Request, RestBindings } from '@loopback/rest';
+import { APPLICATION_CONFIG } from '../application-config';
+import { BaseController } from './base.controller';
+import { Role, User } from '../models';
+import { RoleService, UserService } from '../services';
+import { AttributeProvider, KeyCloackAuthenticator, LoggedError } from '../utils';
 
 export class MeController extends BaseController {
-  constructor() {
+  private _userInfo: object;
+
+  constructor(
+    @inject(RestBindings.Http.REQUEST) private _request: Request,
+    @inject('services.RoleService') private _roleService: RoleService,
+    @inject('services.UserService') private _userService: UserService
+  ) {
     super();
   }
 
-  @get('/api/v1/me', {
-    summary: 'Authenticates a JWT token and returns a User object with Roles',
+  @get('/me', {
+    summary: 'Authenticates a JWT token and returns a User object with its associated Role',
     responses: {
       '200': {
         description: 'Ok',
-        content: {'application/json': {schema: getModelSchemaRef(User)}},
-      },
-    },
+        content: { 'application/json': { schema: getModelSchemaRef(User) } }
+      }
+    }
   })
-  me(): User {
-    console.log('me');
-    return new User();
+  async me(): Promise<User> {
+    const token = this._request.headers.token;
+
+    const auth = new KeyCloackAuthenticator();
+
+    if (typeof token === 'string') {
+      this._userInfo = await auth.authenticate(token);
+    }
+
+    const user: Promise<User> = this.getDatabaseUser();
+
+    return user;
+  }
+
+  async getDatabaseUser(): Promise<User> {
+    // Checks that the selected login field is actually a property of openid client object
+    const loginField = APPLICATION_CONFIG().idp.loginField;
+    if (!this._userInfo.hasOwnProperty(loginField)) {
+      // new LoggedError('Invalid login field for authenticated OpenId client');
+      throw new HttpErrors[500]('Invalid login field for authenticated OpenId client');
+    }
+
+    // Checks that the person is registered in the db
+    const username = this._userInfo[loginField];
+    const users: User[] = await this._userService.get({ where: { username: username } });
+
+    // Case of multiplons, throws
+    if (users.length > 1) {
+      // new LoggedError('Several users registered under the given login field');
+      throw new HttpErrors[500]('Several users registered under the given login field');
+    }
+
+    // Case where the user is not registered in the db
+    let user: User;
+    if (users.length > 0) {
+      user = users[0];
+      const role = user.role;
+    } else {
+      const role: Role = await this._roleService.getById(APPLICATION_CONFIG().misc.default_role_id);
+      user = new User({
+        username: username,
+        role: role
+      });
+    }
+
+    try {
+      const attrProvider = new AttributeProvider(user);
+      attrProvider.updateFromUserInfo(this._userInfo);
+      attrProvider.completeUser();
+    } catch (error) {
+      throw new HttpErrors[500]('Invalid/missing attribute');
+    }
+
+    if (users.length > 0) {
+      this._userService.update(user.id, user);
+    } else {
+      this._userService.save(user);
+    }
+
+    return user;
   }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export * from './logged-error';
 export * from './logger';
+export * from './keycloack-authenticator';
+export * from './attribute-provider';
